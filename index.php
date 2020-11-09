7d5e86b38c764bfed2db78e2e63adacec0fa4176
429273-25 <429273-25>
2020-11-09 14:38:47 +0300

    lab4

--- index.php
+++ index.php
@@ -13,4 +13,30 @@ include "Khoteev/KhoteevException.php";
 
 ini_set("display_errors", 1);
 error_reporting(-1);
+
+
+
+try {
+    $values = array();
+
+    for ($i = 1; $i < 4; $i++) {
+        echo "Введите " . $i . "аргумент : ";
+
+        $values[] = readline();
+    }
+    $va = $values[0];
+    $vb = $values[1];
+    $vc = $values[2];
+
+    $b = new\Khoteev\QuEquation();
+    MyLog::log("Введено уравнение " . $va . "x^2 +  " . $vb . "x + " . $vc);
+    $x = $b->solve($va, $vb, $vc);
+
+    $str = implode(", ", $x);
+    MyLog::log("Корни уравнения: " . $str);
+} catch (KhoteevException $e) {
+    MyLog::log($e->getMessage());
+}
+MyLog::write();
+
 ?>
